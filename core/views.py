fbd79700edb20cebdb04a2090caea11fce48b20a
Leonardo <leo.almeida.silva@hotmail.com>
2017-10-25 15:35:25 -0200

    alterações views

--- core/views.py
+++ core/views.py
@@ -2,22 +2,19 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.template import loader
 from core.models import Aluno
 
 def home(request):
     template = loader.get_template('index.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
+    return HttpResponse(template.render({}, request))
 
 def alunos(request):
     alunos = Aluno.objects.all()
     template = loader.get_template('alunos.html')
-    context = RequestContext(request,{'alunos':alunos})
-    return HttpResponse(template.render(context))
+    return HttpResponse(template.render({'alunos':alunos},request))
 
 def alunoInfo(request,user_id):
 	al = Aluno.objects.get(id=user_id)
 	template = loader.get_template('alunoInfo.html')
-	context = RequestContext(request,{'al':al})
-	return HttpResponse(template.render(context))
+	return HttpResponse(template.render({'al':al},request))
