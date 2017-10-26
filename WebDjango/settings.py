525e73b6dc46eac2e7031eae61af26f0d624cdb2
Leonardo <leo.almeida.silva@hotmail.com>
2017-10-26 19:08:39 -0200

    Alteraçõs bootstrap/js

--- WebDjango/settings.py
+++ WebDjango/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'bootstrap4',
+    'fa',
+    'django_static_jquery',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
--- core/views.py
+++ core/views.py
@@ -23,3 +23,18 @@ def disciplinas(request):
 	disciplinas = Disciplina.objects.all()
 	template = loader.get_template('disciplinas.html')
 	return HttpResponse(template.render({'disciplinas':disciplinas},request))
+
+class ListaAluno():
+    template_name = 'alunos.html'
+    model = Aluno
+    context_object_name = 'alunos'
+	# paginate_by = 100
+ 
+    def get_queryset(self):
+        alunos = Aluno.objects.all()
+        result = self.request.GET.get('pesquisar_por')
+ 
+        # Buscar por usuário
+        if result is not None:
+            alunos = usuarios.filter(username__icontains=result)
+        return alunos
